53ad621f86c89147d674c52f6b70b854cae0b65b
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-11-11 16:11:18 +0500

    feat: User endpoint entity

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly login: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+}
