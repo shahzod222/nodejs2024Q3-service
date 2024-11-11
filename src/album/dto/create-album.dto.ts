1a3eb7fd32b1b1db81abe53dab162ef6de3fb27e
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-11-11 16:38:20 +0500

    featl: Album endpoint entity

---
+++ src/album/dto/create-album.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsNumber()
+  @IsNotEmpty()
+  year: number;
+  @IsString()
+  @IsNotEmpty()
+  artistId: string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.model';
-import { ArtistModule } from './artist/artist.module';
+import { ArtistModule } from './artist/artist.model';
+import { AlbumModule } from './album/album.module';
+
 
 @Module({
-  imports: [UserModule, ArtistModule],
+  imports: [UserModule, ArtistModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
