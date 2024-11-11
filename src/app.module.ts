6577c60a10a52e34357a965d2a304a7f48840c98
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-11-11 16:51:23 +0500

    feat: Track endpoint entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.model';
-import { ArtistModule } from './artist/artist.model';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
-
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, AlbumModule],
+  imports: [UserModule, ArtistModule, AlbumModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/track/dto/create-track.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+export class CreateTrackDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  @IsNotEmpty()
+  artistId: string | null;
+  @IsString()
+  @IsNotEmpty()
+  albumId: string | null;
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+}
